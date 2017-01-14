0e1fdd838d49bb622c6dd42043f831b08281109b
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-01-14 15:26:13 +0100

    Form number input fixed

--- js/form.js
+++ js/form.js
@@ -23,6 +23,24 @@ for (var i = 0; i < apellidosInput.length; i++) {
     });
 }
 
+$(document).ready(function() {
+    $("#ejercito").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+             // Allow: Ctrl+A, Command+A
+            (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || 
+             // Allow: home, end, left, right, down, up
+            (e.keyCode >= 35 && e.keyCode <= 40)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+});
+
 ejercitoInput.addEventListener("keyup", function(event){
     if(this.value > 50) {
         tooManyEnemies.style.display = 'block';
